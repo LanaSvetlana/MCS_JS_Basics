a7e940f0cbe030f57696e615208d3bf42fb9cafd
Svetlana <meteorit-86@yandex.ru>
2018-05-11 11:19:59 +0300

    for git

---
+++ bread&apple/js/script.js
@@ -0,0 +1,10 @@
+console.log('You are at '+window.location);
+
+
+let money = parseFloat(prompt('How much do you have?'))
+let applesCount = parseInt(prompt('How many apples'))
+let breadCount = parseFloat(prompt('How many bread'))
+let sumApples = applesCount * parseFloat(prompt('Apple price'))
+let sumBread = breadCount * parseFloat(prompt('Bread price'))
+let result = money >= (sumApples + sumBread)
+document.body.innerHTML = result
