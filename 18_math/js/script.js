6550064de356e8d174321a8059e4373556d5fc42
Svetlana <meteorit-86@yandex.ru>
2018-05-20 19:54:19 +0300

    !

---
+++ 18_math/js/script.js
@@ -0,0 +1,23 @@
+console.log('You are at '+window.location);
+
+function moduleSquere(num) {
+	return Math.abs(num)/2;
+}
+console.log(moduleSquere(-3));
+console.log(moduleSquere(3));
+
+
+console.log(Math.floor(2.678));
+console.log(Math.round(2.789));
+
+console.log(Math.random())
+
+console.log(Math.floor(Math.random()*(10-5)+5));
+
+
+
+
+function module(num) {
+	return Math.abs(num);
+}
+console.log(Math.abs(Math.random()**Math.floor(Math.random()*(10-1)+1)));
