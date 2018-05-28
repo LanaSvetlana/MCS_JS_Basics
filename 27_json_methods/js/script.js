0aa1631a487ff7c3e27a9b16093ac005a6de725c
Svetlana <meteorit-86@yandex.ru>
2018-05-28 20:13:05 +0300

    ready

---
+++ 27_json_methods/js/script.js
@@ -0,0 +1,9 @@
+console.log('You are at '+window.location);
+
+/*const apiString = '[{"city" : "Moscow","temp" : 10,"pressure" : 250},{"city" : "Miami","temp" : 28,"pressure" : 200},{"city" : "Beiging","temp" : 15,"pressure" : 210}]';
+
+const DATA = JSON.parse(apiString);
+console.log(DATA);*/
+
+const apiString = JSON.stingify(DATA);
+console.log(apiString);
